5d91c9273da4d573d11eb1a430674c9b72ba696d
ahmed hamouda <tito0000469@gmail.com>
2022-02-27 15:32:13 +0200

    dev branch 7

--- lib/main.dart
+++ lib/main.dart
@@ -63,8 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
-    print('first commit for  22333');
+    
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
